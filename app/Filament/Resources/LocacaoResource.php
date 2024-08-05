7f866ec9efadf8fb53ab6135b78414e7994e087d
Wellington Cavalcante <wellarts@gmail.com>
2024-08-05 16:38:29 -0300

    live

--- app/Filament/Resources/LocacaoResource.php
+++ app/Filament/Resources/LocacaoResource.php
@@ -71,14 +71,16 @@ class LocacaoResource extends Resource
                                     ->live()
                                     ->required(false)
                                     ->options(Cliente::all()->pluck('nome', 'id')->toArray())
-                                    ->afterStateUpdated(function ($state, callable $set, Get $get) {
-                                        $cliente = Cliente::find($state);
-                                        Notification::make()
-                                            ->title('ATENÇÃO')
-                                            ->body('A validade da CNH do cliente selecionado: ' . Carbon::parse($cliente->validade_cnh)->format('d/m/Y'))
-                                            ->warning()
-                                            ->persistent()
-                                            ->send();
+                                    ->afterStateUpdated(function ($state) {
+                                        if ($state != null) {
+                                            $cliente = Cliente::find($state);
+                                            Notification::make()
+                                                ->title('ATENÇÃO')
+                                                ->body('A validade da CNH do cliente selecionado: ' . Carbon::parse($cliente->validade_cnh)->format('d/m/Y'))
+                                                ->warning()
+                                                ->persistent()
+                                                ->send();
+                                        }
                                     }),
                                 Forms\Components\Select::make('veiculo_id')
                                     ->required(false)
